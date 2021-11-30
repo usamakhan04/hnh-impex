7a265acc9e87703b6f6be1e08e2eb16dcb7aafbb
ahsan689 <ahsanaleem689@gmail.com>
2021-11-30 13:32:43 +0500

    header updated

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Cards from "./Cards";
 import Header from "./Header";
+import Head from './Head'
 
 const Products = () => {
   return (
     <div>
+      <Head/>
       <Header />
       <div className="container mt-5">
         <Cards />
